fc886dbd6675b995fc9e86a5dd0cf64dbd81443a
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-01-28 18:03:28 +0530

    TDD step: The items in the basket are eligible for discount but current date is after discount end date.

--- src/main/java/com/nelkinda/billingtool/discount/AppleDiscount.java
+++ src/main/java/com/nelkinda/billingtool/discount/AppleDiscount.java
@@ -3,14 +3,49 @@ package com.nelkinda.billingtool.discount;
 import com.nelkinda.billingtool.basket.BasketItem;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Map;
 
 public class AppleDiscount extends Discount {
+
+    /**
+     * Creates an AppleDiscount object with dates injected from properties file.
+     */
+    public AppleDiscount() {
+        super();
+        clock = Clock.fixed(Instant.now(), ZoneId.of("UTC"));
+        // Hardcoding the dates for assignment purpose. Should be loaded from external resource
+        startDate = LocalDate.parse("2021-01-20");
+        endDate = LocalDate.parse("2021-01-31");
+    }
+
+    @Override
+    public void setClock(final Clock clock) {
+        this.clock = clock;
+    }
+
+    @Override
+    public void setStartDate(final LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public void setEndDate(final LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+
     @Override
     public BigDecimal calculateDiscount(final Map<String, BasketItem> basketProductMap,
                                         final BigDecimal total) {
         BigDecimal discountedPrice = total;
-        if (basketProductMap.containsKey("apples")) {
+        final LocalDate currentDate = LocalDate.now(clock);
+        if (basketProductMap.containsKey("apples")
+                && currentDate.isAfter(startDate)
+                && currentDate.isBefore(endDate)) {
             final double discount = 0.01;
             final int totalApples = basketProductMap.get("apples").getQuantity();
             final double amountToReduce = totalApples * discount;
--- src/test/java/com/nelkinda/billingtool/discount/AppleDiscountTest.java
+++ src/test/java/com/nelkinda/billingtool/discount/AppleDiscountTest.java
@@ -1,6 +1,8 @@
 package com.nelkinda.billingtool.discount;
 
 import com.nelkinda.billingtool.basket.BasketItem;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,25 +19,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppleDiscountTest {
 
+    private Discount discount;
+
+    private Map<String, BasketItem> productBasketMap;
+
+    @BeforeEach
+    void setup() {
+        discount = new AppleDiscount();
+        productBasketMap = new ConcurrentHashMap<>();
+    }
+
     @Test
     @DisplayName("The items in the basket are eligible for discount and within the discount date")
     void testAppleDiscountWhenDiscountDateIsValid() {
-        final Map<String, BasketItem> productBasketMap = new ConcurrentHashMap<>();
-        productBasketMap.put(APPLES, new BasketItem(APPLES, "1", BigDecimal.valueOf(0.10), new AppleDiscount()));
-
-        final Discount discount = new AppleDiscount();
-        final Clock clock = Clock.fixed(Instant.parse("2021-01-22T10:15:30.00Z"), ZoneId.of("UTC"));
-        discount.setClock(clock);
-        discount.setStartDate(LocalDate.parse("2021-01-21"));
-        discount.setEndDate(LocalDate.parse("2021-01-23"));
-
+        productBasketMap = createProductBasketMap();
+        setDates("2021-01-22T10:15:30.00Z", "2021-01-21", "2021-01-23");
         final BigDecimal actualTotal = discount.calculateDiscount(productBasketMap, new BigDecimal("0.10"));
         assertEquals(new BigDecimal("0.09"), actualTotal);
+    }
 
+    @Test
+    @DisplayName("The items in the basket are eligible for discount but current date is after discount end date")
+    void testAppleDiscountWhenDiscountDateIsPassed() {
+        productBasketMap = createProductBasketMap();
+        setDates("2021-01-19T10:15:30.00Z", "2021-01-17", "2021-01-18");
+        final BigDecimal actualTotal = discount.calculateDiscount(productBasketMap, new BigDecimal("0.10"));
+        assertEquals(new BigDecimal("0.10"), actualTotal);
+    }
 
+    private Map<String, BasketItem> createProductBasketMap() {
+        productBasketMap = new ConcurrentHashMap<>();
+        productBasketMap.put(APPLES, new BasketItem(APPLES, "1", BigDecimal.valueOf(0.10), new AppleDiscount()));
+        return productBasketMap;
+    }
 
+    private void setDates(final String currentDate, final String startDate, final String endDate) {
+        final Clock clock = Clock.fixed(Instant.parse(currentDate), ZoneId.of("UTC"));
+        discount.setClock(clock);
+        discount.setStartDate(LocalDate.parse(startDate));
+        discount.setEndDate(LocalDate.parse(endDate));
+    }
 
-
+    @AfterEach
+    void tearDown() {
+        discount.setClock(Clock.fixed(Instant.now(), ZoneId.of("UTC")));
     }
 
 }
