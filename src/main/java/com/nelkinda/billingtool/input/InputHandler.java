8bff8f24148d0c9be975a4408f3192aa259a072d
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-01-29 00:38:56 +0530

    Add test for verifying interactions in InputHandler.

--- src/main/java/com/nelkinda/billingtool/input/InputHandler.java
+++ src/main/java/com/nelkinda/billingtool/input/InputHandler.java
@@ -1,17 +1,28 @@
 package com.nelkinda.billingtool.input;
 
+import com.nelkinda.billingtool.basket.BasketCalculator;
+import com.nelkinda.billingtool.basket.BasketItem;
+import com.nelkinda.billingtool.basket.BasketItemFactory;
 import com.nelkinda.billingtool.stock.InMemoryStockItemLoader;
 import com.nelkinda.billingtool.stock.StockItem;
 import com.nelkinda.billingtool.stock.StockItemLoader;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
+@SuppressWarnings("all")
 public class InputHandler {
+
+    private List<StockItem> stockItems;
+    private final List<BasketItem> basket = new ArrayList<>();
+    private final BasketItemFactory itemFactory = new BasketItemFactory(new InMemoryStockItemLoader());
 
     String generateInput() {
         final StockItemLoader stockItemLoader = new InMemoryStockItemLoader();
-        final List<StockItem> stockItems = stockItemLoader.loadStockItems();
+        stockItems = stockItemLoader.loadStockItems();
         final StringBuilder input = new StringBuilder();
         input.append("Please select from the following items\n\n"
                 + "|  **product** | **unit**   | **cost** |\n"
@@ -33,5 +44,53 @@ public class InputHandler {
 
         }
         return input.toString();
+    }
+
+    void handleInput() {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Henry's Grocery");
+        final String textToBeDisplayed = generateInput();
+        while (true) {
+            System.out.println(textToBeDisplayed);
+
+
+            System.out.println("Press the id of the product to select");
+            System.out.println("Press c to calculate Basket");
+            System.out.println("Press q to exit");
+
+            final String productId = scanner.next();
+            if (StringUtils.isNumeric(productId)) {
+                System.out.println("Please select the quantity");
+                final String quantity = scanner.next();
+                for (final StockItem stockItem: stockItems) {
+                    if (stockItem.getProductId() == Integer.parseInt(productId)) {
+                        handleBasket(stockItem.getProductName(), quantity);
+                    }
+                }
+            } else if ("c".equals(productId)) {
+                calculateBasket();
+                scanner.close();
+                return;
+            } else if ("q".equals(productId)) {
+                scanner.close();
+                System.exit(0);
+            }
+
+        }
+    }
+
+    private boolean calculateBasket() {
+        final BasketCalculator calculator = new BasketCalculator();
+        System.out.println("Calculating Basket...");
+        System.out.println("******************");
+        System.out.println("Your total is " + calculator.calculateTotal(basket));
+        System.out.println("******************");
+        System.out.println("Thank you for shopping with us!");
+        return true;
+    }
+
+    private void handleBasket(final String product, final String quantity) {
+        System.out.println("Added " + product + " with quantity " + quantity + " to the basket");
+        basket.add(itemFactory.createBasketItem(product, quantity));
     }
 }
--- src/test/java/com/nelkinda/billingtool/input/InputHandlerTest.java
+++ src/test/java/com/nelkinda/billingtool/input/InputHandlerTest.java
@@ -1,16 +1,43 @@
 package com.nelkinda.billingtool.input;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InputHandlerTest {
+
+    private ByteArrayOutputStream testOut;
+    private PrintStream standardOut;
+
+    private ByteArrayInputStream testIn;
+    private InputStream standardIn;
+
+    private InputHandler inputHandler;
+
+    @BeforeEach
+    void setup() {
+        standardOut = System.out;
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+
+        standardIn = System.in;
+        System.setIn(testIn);
+
+        inputHandler = new InputHandler();
+    }
 
     @Test
     @DisplayName("Returns a list of products to be displayed")
     void testGenerateInput() {
-        final InputHandler handler = new InputHandler();
         final String expectedDisplay = "Please select from the following items\n\n"
                 + "|  **product** | **unit**   | **cost** |\n"
                 + "| :---  | :---: | :---: |\n"
@@ -18,7 +45,29 @@ class InputHandlerTest {
                 + " | 2 | bread | loaf | 0.80 | \n"
                 + " | 3 | milk | bottle | 1.30 | \n"
                 + " | 4 | apples | single | 0.10 | \n";
-        final String actualDisplay = handler.generateInput();
+        final String actualDisplay = inputHandler.generateInput();
         assertEquals(expectedDisplay, actualDisplay);
+    }
+
+    @Test
+    @DisplayName("test interaction when 1 tins of soup is selected")
+    void testInteractionWithOneTinOfSoup() {
+        final String soup = "1 ";
+        final String soupQuantity = "1 ";
+        final String calculate = "c";
+        provideInput(soup + soupQuantity + calculate);
+        inputHandler.handleInput();
+        assertTrue(testOut.toString().contains("0.65"));
+    }
+
+    private void provideInput(final String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+        System.setIn(standardIn);
     }
 }
