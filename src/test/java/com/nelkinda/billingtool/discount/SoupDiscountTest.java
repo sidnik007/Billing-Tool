c46b534fc440e11ce17b89068555f884ab4f1cd2
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-01-28 17:21:26 +0530

    Refactor Test: Move the SoupDiscountTest to discount package.

---
+++ src/test/java/com/nelkinda/billingtool/discount/SoupDiscountTest.java
@@ -0,0 +1,90 @@
+package com.nelkinda.billingtool.discount;
+
+import com.nelkinda.billingtool.basket.BasketItem;
+import com.nelkinda.billingtool.discount.Discount;
+import com.nelkinda.billingtool.discount.NoDiscount;
+import com.nelkinda.billingtool.discount.SoupDiscount;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.nelkinda.billingtool.TestConstants.BREAD;
+import static com.nelkinda.billingtool.TestConstants.SOUP;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SoupDiscountTest {
+
+    private static final String TOTAL = "2.10";
+    private Discount discount;
+
+    @BeforeEach
+    void setup() {
+        discount = new SoupDiscount();
+    }
+
+    @Test
+    @DisplayName("The items in the basket are eligible for discount and within discount date")
+    void discountDateIsValid() {
+        final Map<String, BasketItem> productNameBasketItemMap = createProductBasketMap();
+        setDates("2021-01-22T10:15:30.00Z", "2021-01-21", "2021-01-23");
+        final BigDecimal actualTotal = discount.calculateDiscount(productNameBasketItemMap, new BigDecimal(TOTAL));
+        assertEquals(new BigDecimal("1.70"), actualTotal);
+    }
+
+    @Test
+    @DisplayName("The items in the basket are eligible for discount but current date is after discount end date")
+    void discountDateIsPassed() {
+        final Map<String, BasketItem> productNameBasketItemMap = createProductBasketMap();
+        setDates("2021-01-19T10:15:30.00Z", "2021-01-17", "2021-01-18");
+        final BigDecimal actualTotal = discount.calculateDiscount(productNameBasketItemMap, new BigDecimal(TOTAL));
+        assertEquals(new BigDecimal(TOTAL), actualTotal);
+    }
+
+    @Test
+    @DisplayName("The items in the basket are eligible for discount but current date is before discount start date")
+    void discountDateIsYetToCome() {
+        final Map<String, BasketItem> productNameBasketItemMap = createProductBasketMap();
+        setDates("2021-01-16T10:15:30.00Z", "2021-01-17", "2021-01-18");
+        final BigDecimal actualTotal = discount.calculateDiscount(productNameBasketItemMap, new BigDecimal(TOTAL));
+        assertEquals(new BigDecimal(TOTAL), actualTotal);
+    }
+
+    @Test
+    @DisplayName("The items in the basket are not eligible for discount but within discount date")
+    void basketIsInvalid() {
+        final Map<String, BasketItem> productNameBasketItemMap = new ConcurrentHashMap<>();
+        productNameBasketItemMap.put(BREAD, new BasketItem(BREAD, "1", BigDecimal.valueOf(0.80), new NoDiscount()));
+        setDates("2021-01-22T10:15:30.00Z", "2021-01-21", "2021-01-23");
+        final BigDecimal actualTotal = discount.calculateDiscount(productNameBasketItemMap, new BigDecimal(TOTAL));
+        assertEquals(new BigDecimal(TOTAL), actualTotal);
+    }
+
+    private Map<String, BasketItem> createProductBasketMap() {
+        final Map<String, BasketItem> productNameBasketItemMap = new ConcurrentHashMap<>();
+        productNameBasketItemMap.put(SOUP, new BasketItem(SOUP, "2", BigDecimal.valueOf(0.65), new SoupDiscount()));
+        productNameBasketItemMap.put(BREAD, new BasketItem(BREAD, "1", BigDecimal.valueOf(0.80), new NoDiscount()));
+        return productNameBasketItemMap;
+    }
+
+    private void setDates(final String currentDate, final String startDate, final String endDate) {
+        final Clock clock = Clock.fixed(Instant.parse(currentDate), ZoneId.of("UTC"));
+        discount.setClock(clock);
+        discount.setStartDate(LocalDate.parse(startDate));
+        discount.setEndDate(LocalDate.parse(endDate));
+    }
+
+    @AfterEach
+    void tearDown() {
+        discount.setClock(Clock.fixed(Instant.now(), ZoneId.of("UTC")));
+    }
+
+}
