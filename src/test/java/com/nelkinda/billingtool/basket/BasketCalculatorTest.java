1600eb43990c2e5162bfb9bb068a148a7e6b96a8
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-01-28 14:10:56 +0530

    Refactor Test: Extract common method and common variables.

--- src/test/java/com/nelkinda/billingtool/basket/BasketCalculatorTest.java
+++ src/test/java/com/nelkinda/billingtool/basket/BasketCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.nelkinda.billingtool.basket;
 
 import com.nelkinda.billingtool.stock.InMemoryStockItemLoader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,10 +15,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BasketCalculatorTest {
 
+    private BasketCalculator calculator;
+
+    private BasketItemFactory factory;
+
+    @BeforeEach
+    void setup() {
+        calculator = new BasketCalculator();
+        factory = new BasketItemFactory(new InMemoryStockItemLoader());
+    }
+
     @Test
     @DisplayName("Empty basket returns total as zero")
     void testEmptyBasket() {
-        final BasketCalculator calculator = new BasketCalculator();
         final BigDecimal total = calculator.calculateTotal(Collections.emptyList());
         assertEquals(BigDecimal.ZERO, total);
     }
@@ -25,24 +35,23 @@ class BasketCalculatorTest {
     @Test
     @DisplayName("Basket with one non-discounted item returns the price of the item")
     void testSingleItemQuantityOne() {
-        final BasketCalculator calculator = new BasketCalculator();
-        final BasketItemFactory factory = new BasketItemFactory(new InMemoryStockItemLoader());
         final List<BasketItem> basket = new ArrayList<>(Collections.singletonList(
                 factory.createBasketItem(SOUP, "1")
         ));
-        final BigDecimal actualTotal = calculator.calculateTotal(basket);
-        assertEquals(new BigDecimal("0.65"), actualTotal);
+        assertTotal(basket, "0.65");
     }
 
     @Test
     @DisplayName("Basket with two counts of non-discounted item returns the total price")
     void testSingleItemQuantityTwo() {
-        final BasketCalculator calculator = new BasketCalculator();
-        final BasketItemFactory factory = new BasketItemFactory(new InMemoryStockItemLoader());
         final List<BasketItem> basket = new ArrayList<>(Collections.singletonList(
                 factory.createBasketItem(SOUP, "2")
         ));
+        assertTotal(basket, "1.30");
+    }
+
+    private void assertTotal(final List<BasketItem> basket, final String expectedTotal) {
         final BigDecimal actualTotal = calculator.calculateTotal(basket);
-        assertEquals(new BigDecimal("1.30"), actualTotal);
+        assertEquals(new BigDecimal(expectedTotal), actualTotal);
     }
 }
