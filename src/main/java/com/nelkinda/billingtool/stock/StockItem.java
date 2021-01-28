8de0467726b91845f8e38d4a55c5049ed8e23f5e
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-01-28 13:05:36 +0530

    Refactor: Make the field varialbles of StockItem final.

--- src/main/java/com/nelkinda/billingtool/stock/StockItem.java
+++ src/main/java/com/nelkinda/billingtool/stock/StockItem.java
@@ -7,11 +7,9 @@ import java.math.BigDecimal;
 @Data
 public class StockItem {
 
-    private String productName;
-
-    private String unit;
-
-    private BigDecimal cost;
+    private final String productName;
+    private final String unit;
+    private final BigDecimal cost;
 
     StockItem(final String productName, final String unit, final String cost) {
         this.productName = productName;
