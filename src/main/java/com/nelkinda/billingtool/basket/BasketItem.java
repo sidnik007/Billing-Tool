81ab91d3d93185e6efaa89b8a2482f3e06ae4c28
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-01-28 14:04:51 +0530

    Refactor: Change the quantity to int.

--- src/main/java/com/nelkinda/billingtool/basket/BasketItem.java
+++ src/main/java/com/nelkinda/billingtool/basket/BasketItem.java
@@ -8,12 +8,12 @@ import java.math.BigDecimal;
 public class BasketItem {
 
     private final String productName;
-    private final String quantity;
+    private final int quantity;
     private final BigDecimal cost;
 
     BasketItem(final String productName, final String quantity, final BigDecimal cost) {
         this.productName = productName;
-        this.quantity = quantity;
+        this.quantity = Integer.parseInt(quantity);
         this.cost = cost;
     }
 }
